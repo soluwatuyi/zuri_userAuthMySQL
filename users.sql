70faccfe60bc04b767500b8562ea8255b5df0a87
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-06-13 23:30:20 +0100

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,5 +1,18 @@
-INSERT INTO `students` (`id`, `full_names`, `country`, `email`, `gender`, `password`) VALUES
-(2, 'Nancy Vicky', 'Nigeria', 'nancy@gmail.com', 'Female', 'andy'),
-(3, 'Seyi Olufe', 'Nigeria', 'seyi@gmail.com', 'Male', '1234'),
-(4, 'Chioma Victoria', 'Nigeria', 'vicky@gmail.com', 'Female', '129323'),
-(8, 'Nfon Andrew', 'Cameroon', 'drew@gmail.com', 'Nigeria', 'tatah');
+CREATE TABLE students (
+    id int NOT NULL AUTO_INCREMENT,
+    full_names varchar(255) NOT NULL,
+    country varchar(50) NOT NULL,
+    email varchar(255) NOT NULL,
+    gender varchar(50) NOT NULL,
+    password varchar(250) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
+
+
+INSERT INTO `students` (`full_names`, `country`, `email`, `gender`, `password`) VALUES
+('Nancy Vicky', 'Nigeria', 'nancy@gmail.com', 'Female', 'andy'),
+('Seyi Olufe', 'Nigeria', 'seyi@gmail.com', 'Male', '1234'),
+('Chioma Victoria', 'Nigeria', 'vicky@gmail.com', 'Female', '129323'),
+('Nfon Andrew', 'Cameroon', 'drew@gmail.com', 'Nigeria', 'tatah');
